cbbdaf2c61fee306d61dce91d7eece8c3c40b635
Mutezebra <mutezebra@qq.com>
2024-03-24 18:01:01 +0800

    sytle:rewrite the db test file

--- app/interface/persistence/database/init_test.go
+++ app/interface/persistence/database/init_test.go
@@ -7,7 +7,7 @@ import (
 
 func mysqlInit(t *testing.T) error {
 	t.Helper()
-	dsn := "your dsn"
+	dsn := "root:asodoln1ias@tcp(127.0.0.1:3306)/tiktok?charset=utf8mb4&parseTime=true&loc=Local"
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return err

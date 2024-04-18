1d0dc8708a78808ee430c8c2cd80eebf0a92cb07
Mutezebra <mutezebra@qq.com>
2024-04-19 01:00:56 +0800

    style:add some comments

--- app/domain/service/chat/mq.go
+++ app/domain/service/chat/mq.go
@@ -26,8 +26,9 @@ func (s *Service) EnableMQ() {
 	go s.mq.RunGroupReader(s.ctx, topicName, readerGroup, readerNum, s.mqReadCh)
 }
 
+// writeMsgToMQ 将message发送到mq中
 func (s *Service) writeMsgToMQ(msg *Message) error {
-	data, err := s.getMsgBytes(msg)
+	data, err := s.getMsgBytes(msg) // 转换成二进制格式
 	if err != nil {
 		return err
 	}
@@ -35,10 +36,11 @@ func (s *Service) writeMsgToMQ(msg *Message) error {
 	return nil
 }
 
+// readMsgFromMQ 从kafka中读取message
 func (s *Service) readMsgFromMQ() (*Message, error) {
 	data, ok := <-s.mqReadCh
 	if !ok {
 		return nil, io.EOF
 	}
-	return s.getMsgFromBytes(data)
+	return s.getMsgFromBytes(data) // 将二进制数据序列化成Message
 }
